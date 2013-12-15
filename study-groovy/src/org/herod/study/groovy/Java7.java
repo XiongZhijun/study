edc535716e5d7933642f730e6ddc6693fbd80c8e
XiongZhijun <hust.xzj@gmail.com>
2013-12-15 15:06:50 +0800

    新增Groovy学习工程

---
+++ study-groovy/src/org/herod/study/groovy/Java7.java
@@ -0,0 +1,16 @@
+package org.herod.study.groovy;
+
+public class Java7 {
+	
+	public void test() {
+		int a = 100_00;
+		switch ("hh") {
+		case "bb":
+			System.out.println(a);
+			break;
+		default:
+			break;
+		}
+	}
+
+}
